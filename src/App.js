44f55570fbf1779eb5b8088f28d11bfc00e24d5c
Hien Le <letranduyhien@gmail.com>
2023-04-26 03:28:29 +0300

    Intitialize project with resources, base styles, scss processing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/main.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <div class="container">
+          <div class="flex-grid ">
+            <div class="flex-column col-6 card">
+              <div id="main-score-chart"></div>
+            </div>
+            <div class="flex-column col-3 card">25% width column</div>
+            <div class="flex-column col-3 card">25% width column</div>
+          </div>
+          <div class="flex-grid">
+            <div class="flex-column col-12">100% width column</div>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
